cfeffac3f37939763c713e36ffe712a5ed522199
Inge van Zetten <inge_vzetten@ziggo.nl>
2019-04-28 20:10:14 +0200

    navigatie menu toegevoegd

--- app/src/main/java/com/example/banksysteem/MainActivity.java
+++ app/src/main/java/com/example/banksysteem/MainActivity.java
@@ -1,16 +1,50 @@
 package com.example.banksysteem;
 
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
+
+    BottomNavigationView bottomNav;
+
+    //functie om menu item klik actie
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+            FragmentManager manager = getSupportFragmentManager();
+            switch (item.getItemId()) {
+                case R.id.nav_home:
+                    manager.beginTransaction().replace(R.id.main_fragment_container, new OverzichtFragment()).commit();
+                    break;
+                case R.id.nav_more:
+                    manager.beginTransaction().replace(R.id.main_fragment_container, new MeerFragment()).commit();
+                    break;
+            }
+            return true;
+        }
+    };
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        String test = "hallo";
+        bottomNav = findViewById(R.id.bottom_navigation);
+        bottomNav.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.main_fragment_container, new OverzichtFragment()).commit();
+
 
     }
+
+
 }
