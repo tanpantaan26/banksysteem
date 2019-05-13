0bcb6a22bdc2bde2b7f82f3e5e82c362b9cc73bd
Inge van Zetten <inge_vzetten@ziggo.nl>
2019-05-13 19:49:47 +0200

    Registreerfunctie toegevoegd

--- app/src/main/java/com/example/banksysteem/Controller/AfspraakRegistreerFragment.java
+++ app/src/main/java/com/example/banksysteem/Controller/AfspraakRegistreerFragment.java
@@ -1,32 +1,52 @@
 package com.example.banksysteem.Controller;
 
 import android.app.DatePickerDialog;
-import android.app.TimePickerDialog;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
-import android.widget.TimePicker;
+import android.widget.Spinner;
+import android.widget.Toast;
 
+import com.example.banksysteem.Data.DatabaseConnector;
+import com.example.banksysteem.Model.Afspraak;
+import com.example.banksysteem.Model.Klant;
 import com.example.banksysteem.R;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
+
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
 import java.util.Locale;
 
 public class AfspraakRegistreerFragment extends Fragment {
 
-    private EditText etDatepicker, etTimepicker;
+    private EditText etDatepicker;
     private Calendar calendar;
+    private Klant klant;
+    private DatabaseConnector db = new DatabaseConnector();
+    private String gekozenDatum;
+    private DatePickerDialog datepicker;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+        Bundle klantGegevens = this.getArguments();
+        klant = (Klant) klantGegevens.getSerializable("klantGegevens");
+
         return inflater.inflate(R.layout.afspraak_registreer_fragment, container, false);
     }
 
@@ -34,9 +54,20 @@ public class AfspraakRegistreerFragment extends Fragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        etDatepicker = view.findViewById(R.id.afspraak_etDatum);
-        etTimepicker = view.findViewById(R.id.afspraak_etTijd);
 
+
+        //arraylist met beschikbare tijden
+        ArrayList<String> afspraakTijden = new ArrayList<>();
+        afspraakTijden.add("09:00");
+        afspraakTijden.add("10:00");
+        afspraakTijden.add("11:00");
+
+
+        etDatepicker = view.findViewById(R.id.afspraakfragment_etDatum);
+        String datumVandaag = new SimpleDateFormat("dd-MM-yyyy", Locale.getDefault()).format(new Date());
+        etDatepicker.setText(datumVandaag);
+
+        final Spinner spinnerTijden = view.findViewById(R.id.afsrpaakfragment_spinnerTijden);
 
         ///////////////////////////datepicker///////////////////////////////////
         calendar = Calendar.getInstance();
@@ -44,9 +75,8 @@ public class AfspraakRegistreerFragment extends Fragment {
         final DatePickerDialog.OnDateSetListener date = new DatePickerDialog.OnDateSetListener() {
             @Override
             public void onDateSet(DatePicker datePicker, int year, int monthOfYear, int dayOfMonth) {
-                String date = dayOfMonth + "-" + (monthOfYear +1) + "-" + year;
-                etDatepicker.setText(date);
-
+                gekozenDatum = dayOfMonth + "-" + (monthOfYear + 1 < 10 ? ("0" + (monthOfYear + 1)) : monthOfYear + 1) + "-" + year;
+                etDatepicker.setText(gekozenDatum);
             }
         };
 
@@ -54,36 +84,155 @@ public class AfspraakRegistreerFragment extends Fragment {
         etDatepicker.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                new DatePickerDialog(view.getContext(), date,
+                datepicker = new DatePickerDialog(view.getContext(), date,
                         calendar.get(Calendar.YEAR),
                         calendar.get(Calendar.MONTH),
-                        calendar.get(Calendar.DAY_OF_MONTH)).show();
+                        calendar.get(Calendar.DAY_OF_MONTH));
+
+                datepicker.show();
 
             }
         });
 
-        ///////////////////////////timepicker///////////////////////////////////
 
-        final TimePickerDialog.OnTimeSetListener time = new TimePickerDialog.OnTimeSetListener() {
-            @Override
-            public void onTimeSet(TimePicker timePicker, int hour, int minute) {
-                String time = hour + ":" + minute;
-                etTimepicker.setText(time);
+
+
+        //haal datums op die in de tabel afspraak zitten en kijk welke tijden er al bezet zijn.
+        //Zet de correcte tijden in de spinner
+        //Als er geen tijden meer zijn voor een datum -> voeg datum toe aan datums die niet meer gekozen kunnen worden
+        final String sql = "SELECT * FROM Afspraak WHERE datum = \"" + etDatepicker.getText().toString() + "\"";
+        Log.d("Afspraak", "SQL statement: " + sql);
+        try {
+            DatabaseConnector db = new DatabaseConnector();
+            db.execute(sql);
+            Object oResult = db.get();
+
+            String strResult = oResult.toString();
+            String strResultReplace = strResult.replace("\"", "");
+            Log.d("Afspraak", "strResult: " + strResultReplace);
+
+            if (strResultReplace.equals("msg:select:empty")) {
+
+                ArrayAdapter<String> adapter = new ArrayAdapter<String>(getContext(), android.R.layout.simple_spinner_dropdown_item, afspraakTijden);
+                spinnerTijden.setAdapter(adapter);
+
+            } else {
+
+                Log.d("Afspraak", "strResultTijden: " + strResult);
+
+                JSONArray jsonArray = new JSONArray(strResult);
+                ArrayList<String> tijdenBezet = new ArrayList<>();
+
+                for (int i = 0; i < jsonArray.length(); i++) {
+                    JSONObject jsonObject = (JSONObject) jsonArray.get(i);
+
+                    String tijd = (String) jsonObject.get("Tijd");
+                    tijdenBezet.add(tijd);
+
+                    Log.d("Afspraakfragment", "opgehaalde json: " + tijd);
+                }
+
+                //vergelijk met afsrpaaktijden en verwijder in afsrpaaktijden de tijden uit de database
+                afspraakTijden.removeAll(tijdenBezet);
+
+                if (afspraakTijden.size() > 0){
+                    //Vul de spinner met de tijden die nog wel beschikbaar zijn die datum
+                    ArrayAdapter<String> adapter = new ArrayAdapter<String>(getContext(), android.R.layout.simple_spinner_dropdown_item, afspraakTijden);
+                    spinnerTijden.setAdapter(adapter);
+                }
+                else {
+
+                }
+
+
+
+                //TODO: als de lijst leeg is (of alle tijden al voorkomen in de database -> zet de datum op disabled
+                //TODO: datum disablenen = toevoegen aan lijst Calender[] days
+
             }
-        };
 
-        etTimepicker.setOnClickListener(new View.OnClickListener() {
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+
+        final Button btnVersturen = view.findViewById(R.id.afspraakfragment_btnVersturen);
+        btnVersturen.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                TimePickerDialog timePickerDialog = new TimePickerDialog(
-                        view.getContext(),
-                        time,
-                        Calendar.getInstance().get(Calendar.HOUR),
-                        Calendar.getInstance().get(Calendar.MINUTE), true);
-                timePickerDialog.show();
+
+                //voeg klant en afspraak toe aan database
+                Afspraak afspraak = new Afspraak(klant.getKlantId(), etDatepicker.getText().toString(), spinnerTijden.getSelectedItem().toString());
+
+                Log.d("Afspraak", "Afspraak gegevens: " + afspraak.getDatum() + afspraak.getTijd() + afspraak.getKlantId());
+
+                //insertKlant(klant);
+                //insertAfspraak(afspraak);
             }
         });
+
 
     }
 
+    private void insertKlant(Klant k) {
+
+        String sql = "INSERT INTO Klant(klantID, Voornaam, Achternaam, Telefoon, Email, Adres, Bedrijfsnaam, isGoedgekeurd) " +
+                "VALUES('" + k.getKlantId() + "','" + k.getVoornaam() + "','" + k.getAchternaam()
+                + "','" + k.getTelefoonnummer() + "','" + k.getEmail() + "','" + k.getAdres() + "','" + k.getBedrijfsnaam() + "'," + 0 + ");";
+
+        try {
+            DatabaseConnector db = new DatabaseConnector();
+            db.execute(sql);
+            Log.d("Afspraak", "Db.execute aangeroepen");
+            Object oResult = db.get();
+
+            String strResult = oResult.toString();
+            strResult = strResult.replace("\"", "");
+
+            Log.d("AfspraakFr:insertKlant", "strResult: " + strResult);
+
+            if (strResult.equals("msg:insert:succes")) {
+                Toast.makeText(getContext(), "Klant succesvol toegevoegd", Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(getContext(), "Er is iets misgegaan", Toast.LENGTH_SHORT).show();
+            }
+
+
+        } catch (Exception e) {
+            Log.d("Afspraak", "Er is iets misgegaan");
+            e.printStackTrace();
+        }
+
+    }
+
+    private void insertAfspraak(Afspraak af) {
+
+        String sql = "INSERT INTO Afspraak VALUES('" + af.getDatum() + "','" + af.getTijd() + "','" + af.getKlantId() + "');";
+
+        try {
+            DatabaseConnector db = new DatabaseConnector();
+            db.execute(sql);
+            Object oResult = db.get();
+
+            String strResult = oResult.toString();
+            strResult = strResult.replace("\"", "");
+
+            Log.d("Afspraak:insertAfsrpaak", "strResult: " + strResult);
+
+            if (strResult.equals("msg:insert:succes")) {
+                Toast.makeText(getContext(), "Afspraak succesvol toegevoegd", Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(getContext(), "Er is iets misgegaan", Toast.LENGTH_SHORT).show();
+            }
+
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
+
 }
+
--- app/src/main/java/com/example/banksysteem/Controller/GegevensRegistreerFragment.java
+++ app/src/main/java/com/example/banksysteem/Controller/GegevensRegistreerFragment.java
@@ -1,14 +1,15 @@
 package com.example.banksysteem.Controller;
 
 import android.app.AlertDialog;
+import android.app.DatePickerDialog;
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
-import android.text.TextUtils;
+import android.support.v4.app.FragmentManager;
 import android.util.Log;
-import android.util.Patterns;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -19,13 +20,10 @@ import android.widget.RadioGroup;
 import android.widget.Toast;
 
 import com.example.banksysteem.Data.DatabaseConnector;
+import com.example.banksysteem.Model.Klant;
 import com.example.banksysteem.R;
 import com.example.banksysteem.Util.ValidateAanvraagInput;
 
-import org.json.JSONArray;
-
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 public class GegevensRegistreerFragment extends Fragment {
 
@@ -44,20 +42,6 @@ public class GegevensRegistreerFragment extends Fragment {
     @Override
     public void onViewCreated(@NonNull final View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-
-        String sql = "SELECT * FROM Beheer";
-        try {
-            db.execute(sql);
-            Object oResult = db.get();
-            Log.i("Registreeractivity", "sql result: " + oResult);
-
-            String strResult = oResult.toString();
-            JSONArray jResult = new JSONArray(strResult);
-            Log.i("Registreeractivity", "sql result: " + oResult);
-
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
 
         //invoervelden ophalen
         etVoornaam = view.findViewById(R.id.gegevens_registreer_etVoornaam);
@@ -109,8 +93,9 @@ public class GegevensRegistreerFragment extends Fragment {
             @Override
             public void onClick(View v) {
 
-                //check of de gebruiker een keuze heeft gemaakt tussen particulier en bedrijf
-                if (radioGroup.getCheckedRadioButtonId() == -1){
+                //check of de gebruiker een keuze heeft gemaakt tussen particulier en bedrijf,
+                // zo niet geef dan een melding
+                if (radioGroup.getCheckedRadioButtonId() == -1) {
                     AlertDialog.Builder builder1 = new AlertDialog.Builder(view.getContext());
                     builder1.setMessage("Maak een keuze voor particulier of bedrijf");
 
@@ -125,50 +110,138 @@ public class GegevensRegistreerFragment extends Fragment {
                     AlertDialog alert11 = builder1.create();
                     alert11.show();
 
-                }
 
-                //check of alle velden ingevuld zijn
-                else if(valAvInput.emptyEdittext((ViewGroup) view.findViewById(R.id.gegevens_viewgroup))){
-                    //check of email voldoet aan format aaa@bb.cc
-                    if (!valAvInput.checkEmail(etMail.getText().toString())){
-                        etMail.setError("Voer een geldige email in");
+                } else if (!valAvInput.emptyEdittext((ViewGroup) view.findViewById(R.id.gegevens_viewgroup))) {
+                    Toast.makeText(getContext(), "Vul alle velden in", Toast.LENGTH_LONG).show();
+                } else if (!valAvInput.checkEmail(etMail.getText().toString())) {
+                    etMail.setError("Vul een geldige mail in");
+                } else if (!valAvInput.validateLetters(etVoornaam.getText().toString())) {
+                    etVoornaam.setError("Voornaam mag alleen letters bevatten");
+                } else if (!valAvInput.validateLetters(etAchternaam.getText().toString())) {
+                    etAchternaam.setError("Achternaam mag alleen letters bevatten");
+                } /*else if (!checkKvkBsn(etBSN_KVK.getText().toString())) {
+                    if (particulierRb.isChecked()) {
+                        etBSN_KVK.setError("Vul een geldige bsn in");
+
+                    }
+                    if (bedrijfRb.isChecked()) {
+                        etBSN_KVK.setError("Kvk moet 8 tekens lang zijn");
+
+                    }
+                }*/ else if (!isAlKlant(etBSN_KVK.getText().toString())) {
+
+                } else {
+
+                    //ingevulde waarden ophalen en in klant object zetten om door te sturen naar afspraak scherm
+                    String klantId = etBSN_KVK.getText().toString();
+                    String voornaam = etVoornaam.getText().toString();
+                    String achternaam = etAchternaam.getText().toString();
+                    String telefoonnummer = etTelefoon.getText().toString();
+                    String email = etMail.getText().toString();
+                    String adres = etAdres.getText().toString();
+                    String bedrijfsnaam = etBedrijfsnaam.toString();
+
+                    if (particulierRb.isChecked()) {
+                        bedrijfsnaam = "";
                     }
 
-                    //validate naam
-                    if(!valAvInput.validateLetters(etVoornaam.getText().toString())){
-                        etVoornaam.setError("Voornaam mag alleen letters bevatten");
-                    };
-                    if(!valAvInput.validateLetters(etAchternaam.getText().toString())){
-                        etAchternaam.setError("Achternaam mag alleen letters bevatten");
-                    };
+                    Klant klant = new Klant(klantId, voornaam, achternaam, telefoonnummer, email, adres, bedrijfsnaam);
 
-                    //validate bsn en kvk
-                    checkKvkBsn(etBSN_KVK.getText().toString());
-                    Toast.makeText(getContext(), "Alle tekstvelden zijn gevuld", Toast.LENGTH_SHORT).show();
-                }else{
-                    Toast.makeText(getContext(), "lege velden", Toast.LENGTH_SHORT).show();
+                    Log.d("GegevensRegistreer", "klant: " + klant.getKlantId());
+
+                    String sql = "SELECT * FROM ZwarteLijst WHERE KlantklantID = '" + klantId + "';";
+                    try {
+                        DatabaseConnector db2 = new DatabaseConnector();
+                        db2.execute(sql);
+                        Object oResult = db.get();
+
+                        String strResult = oResult.toString();
+                        strResult = strResult.replace("\"", "");
+                        Log.d("Registreeractivity", "strResult: " + strResult);
+
+                        if (strResult.equals("msg:select:empty")) {
+                            FragmentManager fragmentManager = getActivity().getSupportFragmentManager();
+
+                            Bundle klantGegevens = new Bundle();
+                            klantGegevens.putSerializable("klantGegevens", klant);
+                            AfspraakRegistreerFragment afspraakFragment = new AfspraakRegistreerFragment();
+                            afspraakFragment.setArguments(klantGegevens);
+                            fragmentManager.beginTransaction().replace(R.id.registreeractivity_fragment_container, afspraakFragment).commit();
+
+                        } else {
+                            AlertDialog.Builder builder1 = new AlertDialog.Builder(view.getContext());
+                            builder1.setMessage("U bent afgewezen om klant te worden bij deze bank");
+
+                            builder1.setPositiveButton(
+                                    "OK",
+                                    new DialogInterface.OnClickListener() {
+                                        public void onClick(DialogInterface dialog, int id) {
+                                            Intent intent = new Intent(getContext(), LoginActivity.class);
+                                            startActivity(intent);
+
+                                        }
+                                    });
+                            AlertDialog alert11 = builder1.create();
+                            alert11.show();
+                        }
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+
                 }
 
-                //getFragmentManager().beginTransaction().replace(R.id.registreeractivity_fragment_container, new AfspraakRegistreerFragment()).commit();
             }
+
         });
     }
 
-    private void checkKvkBsn(String kvk_bsn){
+    //check of bsn voldoet aan 11 proef en of kvk niet langer is dan 8 tekens
+    private boolean checkKvkBsn(String kvk_bsn) {
 
-        if (particulierRb.isChecked()){
+        if (particulierRb.isChecked()) {
             int bsn = Integer.parseInt(kvk_bsn);
-            if(!valAvInput.isValidBSN(bsn)){
-                etBSN_KVK.setError("Vul een geldige bsn in");
+            if (!valAvInput.isValidBSN(bsn)) {
+                return false;
             }
         }
-        if (bedrijfRb.isChecked()){
-            if (kvk_bsn.length() != 8){
-                etBSN_KVK.setError("Kvk moet 8 karakters lang zijn");
+        if (bedrijfRb.isChecked()) {
+            if (kvk_bsn.length() != 8) {
+                return false;
             }
         }
+        return true;
+    }
+
+
+    private boolean isAlKlant(String klantId) {
+
+
+        String sql = "SELECT * FROM Klant WHERE Klantid = '" + klantId + "';";
+        String strResult = "";
+
+        try {
+
+            db.execute(sql);
+            Object oResult = db.get();
+
+            strResult = oResult.toString();
+            strResult = strResult.replace("\"", "");
+
+            Log.d("isAlKlant", "strResult: " + strResult);
+
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        if (strResult.equals("msg:select:empty")) {
+            return true;
+        } else {
+            Toast.makeText(getContext(), "U bent al bekend bij de bank", Toast.LENGTH_LONG).show();
+            return false;
+        }
+
 
     }
 
 }
-
