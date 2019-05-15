191752816aef921efdedb62ad95d7db1bd00c517
Rutgervdh95 <rutgervdh95@gmail.com>
2019-05-15 13:12:36 +0200

    Boeking fragment framework gemaakt

--- app/src/main/java/com/example/banksysteem/Controller/BoekingFragment.java
+++ app/src/main/java/com/example/banksysteem/Controller/BoekingFragment.java
@@ -1,24 +1,33 @@
 package com.example.banksysteem.Controller;
 
+import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
 
 import com.example.banksysteem.R;
 
 public class BoekingFragment extends Fragment {
+    public Button verzend;
 
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.profiel_fragment, container, false);
+        return inflater.inflate(R.layout.boeking_fragment, container, false);
     }
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        verzend = getView().findViewById(R.id.verzend);
+        verzend.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getActivity().getApplicationContext(), "Verstuurd", Toast.LENGTH_LONG).show();
+            }
+        });
         super.onViewCreated(view, savedInstanceState);
     }
 }
