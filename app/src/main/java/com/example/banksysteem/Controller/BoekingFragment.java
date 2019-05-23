9f977c19398930f0cc7a77eea82fd47e301a9df4
Rutgervdh95 <rutgervdh95@gmail.com>
2019-05-23 10:45:40 +0200

    Boeking fragment gefixt

--- app/src/main/java/com/example/banksysteem/Controller/BoekingFragment.java
+++ app/src/main/java/com/example/banksysteem/Controller/BoekingFragment.java
@@ -5,14 +5,20 @@ import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.text.Editable;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import com.example.banksysteem.Data.DatabaseConnector;
 import com.example.banksysteem.R;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.util.concurrent.ExecutionException;
 
@@ -28,6 +34,9 @@ public class BoekingFragment extends Fragment {
     public Object zResult;
     public Object oResult;
     public String sql;
+    public String jsonSaldoZend;
+    public String jsonSaldoOntvang;
+    JSONArray jResult;
 
 
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
@@ -40,28 +49,42 @@ public class BoekingFragment extends Fragment {
         verzend.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                
-                Editable saldoInput = getView().findViewById(R.id.saldoInput);
-                saldoUit = Double.valueOf(saldoInput.toString());
 
-                Editable ontvangerInput = getView().findViewById(R.id.saldoInput);
+                EditText saldoInput = getView().findViewById(R.id.saldoInput);
+                saldoUit = Double.valueOf(saldoInput.getText().toString());
+
+                EditText ontvangerInput = getView().findViewById(R.id.ontvangerInput);
                 String ontvanger = ontvangerInput.toString();
 
                 dbz = new DatabaseConnector();
-                sql = "SELECT Saldo FROM Rekening WHERE Rekeningnummer = '2147483647'";
+                sql = "SELECT Saldo FROM Rekening WHERE Rekeningnummer = '123456789'";
                 dbz.execute(sql);
                 try {
                     zResult = dbz.get();
+                    String strResult = zResult.toString();
+                    jResult = new JSONArray(strResult);
                 } catch (ExecutionException e) {
                     e.printStackTrace();
                 } catch (InterruptedException e) {
                     e.printStackTrace();
+                } catch (JSONException e) {
+                    e.printStackTrace();
                 }
-                saldoZend = Double.valueOf(zResult.toString());
+                for (int i = 0; i < jResult.length(); i++) {
+                    try {
+                        JSONObject jsonObject = (JSONObject) jResult.get(i);
+                        jsonSaldoZend = (String) jsonObject.get("Saldo");
+
+                        Log.i("e", jResult.toString());
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                }
+                saldoZend = Double.valueOf(jsonSaldoZend);
 
                 Double saldoZendNieuw = saldoZend - saldoUit;
                 dbv = new DatabaseConnector();
-                sql = "UPATE Rekening SET Saldo = '"+saldoZendNieuw+"' WHERE Rekeningnummer = '2147483647'";
+                sql = "UPATE Rekening SET Saldo = '"+saldoZendNieuw+"' WHERE Rekeningnummer = '123456789'";
                 dbv.execute(sql);
 
 
@@ -70,12 +93,26 @@ public class BoekingFragment extends Fragment {
                 dbo.execute(sql);
                 try {
                     oResult = dbo.get();
+                    String strOResult = oResult.toString();
+                    jResult = new JSONArray(strOResult);
                 } catch (ExecutionException e) {
                     e.printStackTrace();
                 } catch (InterruptedException e) {
                     e.printStackTrace();
+                } catch (JSONException e) {
+                    e.printStackTrace();
                 }
-                saldoOntvang = Double.valueOf(oResult.toString());
+                for (int i = 0; i < jResult.length(); i++) {
+                    try {
+                        JSONObject jsonObject = (JSONObject) jResult.get(i);
+                        jsonSaldoOntvang = (String) jsonObject.get("Saldo");
+
+                        Log.i("e", jResult.toString());
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                }
+                saldoOntvang = Double.valueOf(jsonSaldoOntvang);
 
                 Double saldoOntvangNieuw = saldoOntvang + saldoUit;
                 dbs = new DatabaseConnector();
